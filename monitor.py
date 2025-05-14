ce13215c23c20a0bf22f037f209ba7ca42f4db8c
WORLDBUS <masood_y@rocketmail.com>
2025-05-14 20:41:03 +0200

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,15 +1,12 @@
-# monitor.py
 import subprocess
 
-def run_tests(domain: str) -> dict:
-    #result ping
+def run_ping_traceroute(domain: str) -> str:
     ping = subprocess.run(
-      ["ping", "-c", "3", domain],
-      capture_output=True, text=True
+        ["ping", "-c", "3", domain],
+        capture_output=True, text=True
     ).stdout
-    # result traceroute
     trace = subprocess.run(
-      ["traceroute", "-m", "10", domain],
-      capture_output=True, text=True
+        ["traceroute", "-m", "10", domain],
+        capture_output=True, text=True
     ).stdout
-    return {"ping": ping, "traceroute": trace}
+    return f"Ping Results:\n{ping}\nTraceroute Results:\n{trace}"
